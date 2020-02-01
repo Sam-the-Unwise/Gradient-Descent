d76c054b857374e4e04870506c381610a594b62b
Jamp-H <jmh824@nau.edu>
2020-02-01 14:46:10 -0700

    almost there

--- gradientDescent.py
+++ gradientDescent.py
@@ -1,31 +1,63 @@
-import numpy
+import numpy as np
 
 # function that does gradient descent
-# INPUT ARGS: 
-#   X : a mtrix of numeric inputs
-#   y : a vector of binary outputs
+# INPUT ARGS:
+#   X : a matrix of numeric inputs {Obervations x Feature}
+#   y : a vector of binary outputs {0,1}
 #   stepSize : learning rate - epsilon parameters
 #   maxIterations : pos int that controls how many steps to take
-def gradientDescent(X, y, stepSize, maxIterations):
+def gradientDescent(X, y, stepSize, max_iterations):
 
     # VARIABLES
 
+    # tuple of array dim (row, col)
+    arr_dim = X.shape
+
+    # num of input features
+    X_arr_col = arr_dim[1]
+
+    wm_total_entries = X_arr_col * max_iterations
     # variable that initiates to the zero vector
-    weightVector = array([0, 0])
+    weight_vector = np.zeros(X_arr_col)
 
     # matrix for real numbers
     #   row of #s = num of inputs
     #   num of cols = maxIterations
-    weightMatrix = [len(X)][maxIterations]
+    weight_matrix = np.array(np
+                        .zeros(wm_total_entries)
+                        .reshape(X_arr_col, max_iterations))
+
 
     # ALGORITHM
+    for(index in range(maxIterations)):
+        #calculate y_tid
+        y_tild = -1
 
-    # include a for loop over iterations (from 1 to maxIterations)
-    # during each iteration
-    #   first comput the gradient give the current weightVector
-    #   then update weightVector by taking a step in the negative gradient direction
-    #   then store the resulting weightVector in the corr. column of weightMatrix
+        if(y == 1):
+            y_tild = 1
+
+        gradient = (1/(1+numpy.exp((-y_tild)*weight_vector*X[index]))*
+                    (numpy.exp((-y_tild)*weight_vector*X[index]))*
+                    ((-y_tild)*X[index]))
+
+        # update weight_vector depending on positive or negative
+        # If negative, you add to the steps
+        weight_vector +=
+        # If positive, you subtract it
+
+        # store the resulting weight_vector in the corresponding column weight_matrix
+        weight_matrix.item((index, 1)) = weight_vector
+
+
+
+
+    # compute gradiant
+    #
 
     # end of algorithm
-    return weightMatrix
+    return weight_matrix
 
+
+matrix = np.array([[0,0],[0,0],[0,0]])
+
+gradientDescent(matrix,0,0,5)
