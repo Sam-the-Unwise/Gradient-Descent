660ddd9d81954529a0e72f8289d7874f9a710402
Samantha Muellner <shm53@nau.edu>
2020-02-07 11:32:29 -0700

    60-20-20 finished, working on matrix multiply
    Trying to get matrix multiply to work. Graphing has not yet been started

--- gradientDescent.py
+++ gradientDescent.py
@@ -13,15 +13,16 @@ def gradientDescent(X, y, step_size, max_iterations):
 
     # VARIABLES
 
-    # tuple of array dimensions: (row, col)
+    # tuple of array dim (row, col)
     arr_dim = X.shape
+    #print(arr_dim)
 
     # num of input features
     X_arr_col = arr_dim[1]
 
     wm_total_entries = X_arr_col * max_iterations
 
-    # variable that initiates to the weight vector -- should start as zero vector
+    # variable that initiates to the weight vector
     weight_vector = np.zeros(X_arr_col)
 
     # matrix for real numbers
@@ -31,37 +32,38 @@ def gradientDescent(X, y, step_size, max_iterations):
                         .zeros(wm_total_entries)
                         .reshape(X_arr_col, max_iterations))
 
-
     # ALGORITHM
-    for index in range(0, max_iterations):
-        # calculate y tilda
-        y_tild = -1
+    weight_vector_transpose = np.transpose(weight_vector)
 
-        if(y[index] == 1):
-            y_tild = 1
+    for iteration in range(0, max_iterations):
+        #calculate y_tid
+        for index in range(0, X.shape[1]):
 
+            grad_log_losss = 0
+            verctor_mult = 0
+            inner_exp = 0
 
-        # variables for simplification
-        weight_vector_transpose = np.transpose(weight_vector)
-        vector_mult = np.multiply(weight_vector_transpose, X[index,:])
-        inner_exp = np.multiply(y_tild, vector_mult)
-        
-        # calculate gradient
-        gradient = (1/(1+np.exp(inner_exp))*
-                    (np.exp(inner_exp))*
-                    ((-y_tild)*X[index,:]))
+            y_tild = -1
 
+            if(y[index] == 1):
+                y_tild = 1
+
+            # variables for simplification
+            verctor_mult = np.multiply(weight_vector_transpose, X[index,:])
+            inner_exp = np.multiply(y_tild, verctor_mult)
+            # calculate gradient
+
+            gradient = (1/(1+np.exp(inner_exp))*
+                        (np.exp(inner_exp))*
+                        ((-y_tild)*X[index,:]))
+
+            grad_log_losss += gradient
+
+        mean_grad_log_loss = grad_log_losss/X.shape[1]
 
         # update weight_vector depending on positive or negative
-        # If negative, you add to the steps
-        if y_tild == -1:
-            weight_vector += gradient
+        weight_vector = weight_vector - np.multiply(step_size, mean_grad_log_loss)
 
-        # If positive, you subtract it
-        elif y_tild == 1:
-            weight_vector -= gradient
-
-        #print(weight_vector)
         # store the resulting weight_vector in the corresponding column weight_matrix
         weight_matrix[: ,index] = weight_vector
 
@@ -91,11 +93,8 @@ def scale(matrix):
 
         std = sqrt(col_sq_sum/col_size)
 
-        #print(f"{counter} : mean {mean} std: {std}")
         column -= mean
-        #print(f"col - {column}\n")
         column /= std
-        #print(f"col / {column}")
 
 # Function: convert_data_to_matrix
 # INPUT ARGS:
@@ -106,8 +105,170 @@ def convert_data_to_matrix(file_name):
         spam_file = list(csv.reader(data_file, delimiter = " "))
 
     data_matrix_full = np.array(spam_file[0:], dtype=np.float)
-
     return data_matrix_full
+
+
+
+# Function: calculate_train_test_and_val_data
+# INPUT ARGS:
+#   data_matrix_test : modified data matrix (lacking last row of 1s and 0s)
+#   data_matrix_full : original data matrix
+#   binary_vector : vector containing the list of 1s and 0s in correspondance to
+#       data_matrix_test (obtained from data_matrix_full)
+# Return: [none]
+def calculate_train_test_and_val_data(data_matrix_test, data_matrix_full, binary_vector):
+    # get 60-20-20 of data for train-validation-test
+
+    row_length = data_matrix_full.shape[0]
+    col_length = data_matrix_full.shape[1]
+
+    # divide num of zero items into 60-20-20
+    train_data_count = int(row_length * .6)
+    val_data_count = int(row_length * .2)
+    test_data_count = int(row_length * .2)
+    
+    # create zero-arrays to initialize the numpy array for each set
+    #   allows us to add the arrays from the data_matrix_full since they will now
+    #   be the same size
+    array_of_zeros = []
+
+    for i in range(col_length - 1):
+        array_of_zeros.append(0)
+
+    train_data = np.array(array_of_zeros)
+    val_data = np.array(array_of_zeros)
+    test_data = np.array(array_of_zeros)
+    
+    # NOTE: to do this, we must assume that the data_matrix_test is still in the same order as the binary_vector#
+
+    # create index count that will keep track of the index we're at in the data_matrix_test and binary_vector
+    index_count = 0
+
+    # create count that will count the total number of zero-data we've added
+    zero_test_data_count = 0
+    zero_train_data_count = 0
+    zero_val_data_count = 0
+    one_test_data_count = 0
+    one_train_data_count = 0
+    one_val_data_count = 0
+
+    train_binary_vector = []
+    test_binary_vector = []
+    val_binary_vector = []
+
+    # loop through matrix and assign certain items to different matrixes
+    #       assign 60 to train, 20 to val, and 20 to test for both 0s and 1s
+    for data_list in data_matrix_test:
+
+        # append the first 60% of data to the zero_train_data
+        if index_count < train_data_count:
+            train_data = np.vstack((train_data, np.array(data_list)))
+
+            if int(binary_vector[index_count]) is 0:
+                zero_train_data_count += 1
+            else:
+                one_train_data_count += 1
+
+            train_binary_vector.append(binary_vector[index_count])
+
+        # append the next 20% of data to the zero_val_data
+        elif index_count < (train_data_count + val_data_count):
+            val_data = np.vstack((val_data, np.array(data_list)))
+
+            if int(binary_vector[index_count]) is 0:
+                zero_val_data_count += 1
+            else:
+                one_val_data_count += 1
+
+            val_binary_vector.append(binary_vector[index_count])
+            
+        # append the last 20% of data to the zero_test_data
+        elif index_count < (train_data_count + val_data_count + test_data_count):
+            test_data = np.vstack((test_data, np.array(data_list)))
+
+            if int(binary_vector[index_count]) is 0:
+                zero_test_data_count += 1
+            else:
+                one_test_data_count += 1
+
+            test_binary_vector.append(binary_vector[index_count])
+
+        index_count += 1
+
+    # get ride of added array of 0s we made when initializing array
+    train_data = np.delete(train_data, 0, 0)
+    val_data = np.delete(val_data, 0, 0)
+    test_data = np.delete(test_data, 0, 0)
+
+    # print out amount of 0s and 1s in each set
+    print("                y")
+    print("set              0     1")
+    print("test           " + str(zero_test_data_count) + "  " + str(one_test_data_count))
+    print("train          " + str(zero_train_data_count) + "  " + str(one_train_data_count))
+    print("validation     " + str(zero_val_data_count) + "  " + str(one_val_data_count))
+
+
+    # apply gradient descent to train data
+    weight_matrix = gradientDescent(train_data, train_binary_vector, .05, 500)
+
+    # save our weight matrix
+    np.savetxt("plzwork.csv", weight_matrix, delimiter = " ")
+    data_matrix_full = convert_data_to_matrix("spam.data")
+
+    temp_matrix = []
+    temp_full_matrix = []
+    for i in range(weight_matrix.shape[1] - 1):
+        temp_matrix.append(0)
+    
+    for j in range(train_data.shape[0] - 1):
+        temp_full_matrix.append([temp_matrix])
+
+
+    # multiply weight_matrix with train_matrix and val_matrix
+    multi_train_data = np.array(temp_full_matrix)#np.zeros(weight_matrix.shape[1]*train_data.shape[0]))
+    multi_val_data = []
+
+    # variable to access the correct column of the weight matrix for multiplication
+    w_matrix_column = 0
+
+    # # loop through rows of train_matrix
+    # for row in train_data:
+    #     w_matrix_row = 0
+    #     dot_product_sum = 0
+
+    #     for number in row:
+    #         # get the element in the column of the weight matrix that is equal to this
+    #         #   matrices row
+    #         weight_matrix_multiplier = weight_matrix[w_matrix_row][w_matrix_column]
+
+    #         dot_product_sum += number * weight_matrix_multiplier
+
+    #     multi_train_data.append(dot_product_sum)
+
+    #     w_matrix_column += 1
+
+    #     # print(weight_matrix[w_matrix_row][row])
+
+    #     #loop through columns of train data
+    #         # multiply each item in corespondance with the item in weight matrix
+    #         # add all items together
+
+    #     # loop through columns of val data
+    #         # multiply each item in corespondance with the item in weight matrix
+    #         # add all items together
+    # print(multi_train_data)
+
+    np.matmul(train_data, weight_matrix, out = multi_train_data)
+    print(multi_train_data)
+    
+        
+
+    print(weight_matrix.shape)
+    print(train_data.shape)
+    print(val_data.shape)
+
+
+
 
 # Function: main
 # INPUT ARGS:
@@ -117,96 +278,22 @@ def main():
     # get the data from our CSV file
     data_matrix_full = convert_data_to_matrix("spam.data")
 
+    np.random.shuffle(data_matrix_full)
+
     # get necessary variables
     # shape yields tuple : (row, col)
     col_length = data_matrix_full.shape[1]
 
-    data_matrix_test = np.delete(data_matrix_full, col_length - 1, 1) # delete last column
+    data_matrix_test = np.delete(data_matrix_full, col_length - 1, 1)
+
     binary_vector = data_matrix_full[:,57]
 
-    test_col_length = data_matrix_full.shape[1]
-
-    # scale our matrix so our data is between 0 and 1
     scale(data_matrix_test)
 
-    # get 60-20-20 of data for train-validation-test
-
-    # go through and count items in matrix belonging to 0 and 1 groups
-    count_of_zeros = 0
-    count_of_ones = 0
-
-    for item in data_matrix_full:
-        if int(item[col_length - 1]) == 1:
-            count_of_ones += 1
-
-        if int(item[col_length - 1]) == 0:
-            count_of_zeros += 1
-
-    print(count_of_zeros)
-    # print(count_of_ones)
-
-    # divide num of zero items into 60-20-20
-    zero_train_data_count = count_of_zeros * .6
-    print(zero_train_data_count)
-    zero_val_data_count = count_of_zeros * .2
-    print(zero_val_data_count)
-    zero_test_data_count = count_of_zeros * .2
-    print(zero_test_data_count)
-
-    # divide num of one items into 60-20-20
-    one_train_data_count = int(count_of_ones * .6)
-    one_val_data_count = int(count_of_ones * .2)
-    one_test_data_count = int(count_of_ones * .2)
-
-    # loop through matrix and assign certain items to different matrixes
-    #       assign 60 to train, 20 to val, and 20 to test
-    zero_train_data = np.array([])
-    zero_val_data = np.array([])    
-    zero_test_data = np.array([])
-
-    # to do this, we must assume that the data_matrix_test is still in the same order as the binary_vector#
-
-    # create index count that will keep track of the index we're at in the data_matrix_test and binary_vector
-    index_count = 0
-    # create count that will count the total number of zero-data we've added
-    zero_count = 0
-
-    for data_list in data_matrix_test:
-        # check if this row is one of 0 data by checking binary_vector
-        if int(binary_vector[index_count]) is 0:
-
-            # append the first 60% of data to the zero_train_data
-            if zero_count < zero_train_data_count:
-                zero_train_data = np.append(zero_train_data, [data_list])
-
-            # append the next 20% of data to the zero_val_data
-            elif zero_count < (zero_train_data_count + zero_val_data_count):
-                zero_val_data = np.append(zero_val_data, data_list)
-            
-            # append the last 20% of data to the zero_test_data
-            elif zero_count < (zero_train_data_count + zero_val_data_count + zero_test_data_count):
-                zero_test_data = np.append(zero_test_data, data_list)
-
-            zero_count += 1
-
-        index_count += 1
-
-    for item in zero_train_data:
-        print(item)
-
-
-
-
-    one_train_data = np.array(np.zeros(one_train_data_count))
-    one_val_data = np.array(np.zeros(one_val_data_count))
-    one_test_data = np.array(np.zeros(one_test_data_count))
-
-
-    # np.random.shuffle(data_matrix_test)
-
-    # apply gradient descent to matrix
-    learned_weight_matrix = gradientDescent(data_matrix_test, binary_vector, .05, 100)
-    
+    # calculate train, test, and validation data
+    weight_matrix = calculate_train_test_and_val_data(data_matrix_test, 
+                                                    data_matrix_full, 
+                                                    binary_vector)
 
 
 
