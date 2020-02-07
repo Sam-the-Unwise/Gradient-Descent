58b2f6e0b4cc568bb51c614a1915fd8ad8afbd7c
Samantha Muellner <shm53@nau.edu>
2020-02-06 17:10:29 -0700

    Dividing data
    Dividing data into 60-20-20 segments. Not complete. Needs actual data added to matrixes for test and train for 0 and 1 data

--- gradientDescent.py
+++ gradientDescent.py
@@ -1,5 +1,6 @@
 import numpy as np
 import csv
+from math import sqrt
 
 # Function: gradientDescent
 # INPUT ARGS:
@@ -12,7 +13,7 @@ def gradientDescent(X, y, step_size, max_iterations):
 
     # VARIABLES
 
-    # tuple of array dim (row, col)
+    # tuple of array dimensions: (row, col)
     arr_dim = X.shape
 
     # num of input features
@@ -20,7 +21,7 @@ def gradientDescent(X, y, step_size, max_iterations):
 
     wm_total_entries = X_arr_col * max_iterations
 
-    # variable that initiates to the weight vector
+    # variable that initiates to the weight vector -- should start as zero vector
     weight_vector = np.zeros(X_arr_col)
 
     # matrix for real numbers
@@ -33,18 +34,19 @@ def gradientDescent(X, y, step_size, max_iterations):
 
     # ALGORITHM
     for index in range(0, max_iterations):
-        #calculate y_tid
+        # calculate y tilda
         y_tild = -1
 
         if(y[index] == 1):
             y_tild = 1
 
+
         # variables for simplification
         weight_vector_transpose = np.transpose(weight_vector)
-        verctor_mult = np.multiply(weight_vector_transpose, X[index,:])
-        inner_exp = np.multiply(y_tild, verctor_mult)
+        vector_mult = np.multiply(weight_vector_transpose, X[index,:])
+        inner_exp = np.multiply(y_tild, vector_mult)
+        
         # calculate gradient
-
         gradient = (1/(1+np.exp(inner_exp))*
                     (np.exp(inner_exp))*
                     ((-y_tild)*X[index,:]))
@@ -67,37 +69,134 @@ def gradientDescent(X, y, step_size, max_iterations):
     # end of algorithm
     return weight_matrix
 
+# Function: scale
+# INPUT ARGS:
+#   matrix : the matrix that we need to scale
+# Return: [none]
 def scale(matrix):
     matrix_t = np.transpose(matrix)
-    col_sq_sum = 0
+    counter = 0
+
     for column in matrix_t:
+        counter += 1
+        col_sq_sum = 0
+
         sum = np.sum(column)
         shape = column.shape
-        size = shape[0]
-        mean = sum/size
+        col_size = shape[0]
+        mean = sum/col_size
+
         for item in column:
-            col_sq_sum += (item - mean)**2
-        std = col_sq_sum/size
+            col_sq_sum += ((item - mean)**2)
+
+        std = sqrt(col_sq_sum/col_size)
+
+        #print(f"{counter} : mean {mean} std: {std}")
         column -= mean
+        #print(f"col - {column}\n")
         column /= std
+        #print(f"col / {column}")
 
-
+# Function: convert_data_to_matrix
+# INPUT ARGS:
+#   file_name : the csv file that we will be pulling our matrix data from
+# Return: data_matrix_full
 def convert_data_to_matrix(file_name):
     with open(file_name, 'r') as data_file:
         spam_file = list(csv.reader(data_file, delimiter = " "))
 
     data_matrix_full = np.array(spam_file[0:], dtype=np.float)
+
     return data_matrix_full
 
-data_matrix_full = convert_data_to_matrix("spam.data")
+# Function: main
+# INPUT ARGS:
+#   [none]
+# Return: [none]
+def main():
+    # get the data from our CSV file
+    data_matrix_full = convert_data_to_matrix("spam.data")
 
-data_matrix_test = np.delete(data_matrix_full, -1, 1)
+    # get necessary variables
+    # shape yields tuple : (row, col)
+    col_length = data_matrix_full.shape[1]
 
-binary_vector = data_matrix_full[:,57]
+    # count = 0
+    # for element in data_matrix_full:
+    #     if count is 0:
+    #         print(element)
+    #     count += 1
 
-scale(data_matrix_test)
+    data_matrix_test = np.delete(data_matrix_full, col_length - 1, 1) # delete last column
+    binary_vector = data_matrix_full[:,57]
+
+    # count = 0
+    # for element in data_matrix_test:
+    #     if count is 0:
+    #         print(element)
+    #     count += 1
+
+    test_col_length = data_matrix_full.shape[1]
+
+    # scale our matrix so our data is between 0 and 1
+    scale(data_matrix_test)
+
+    # get 60-20-20 of data for train-validation-test
+
+    # go through and count items in matrix belonging to 0 and 1 groups
+    count_of_zeros = 0
+    count_of_ones = 0
+
+    for item in data_matrix_full:
+        if int(item[col_length - 1]) == 1:
+            count_of_ones += 1
+
+        if int(item[col_length - 1]) == 0:
+            count_of_zeros += 1
+
+    # print(count_of_zeros)
+    # print(count_of_ones)
+
+    # divide num of zero items into 60-20-20
+    zero_train_data_count = count_of_zeros * .6
+    zero_val_data_count = count_of_zeros * .2
+    zero_test_data_count = count_of_zeros * .2
+
+    # divide num of one items into 60-20-20
+    one_train_data_count = count_of_ones * .6
+    one_val_data_count = count_of_ones * .2
+    one_test_data_count = count_of_ones * .2
+
+    # loop through matrix and assign certain items to different matrixes
+    #       assign 60 to train, 20 to val, and 20 to test
+    zero_train_data = np.array(np
+                        .zeros(wm_total_entries)
+                        .reshape(X_arr_col, max_iterations))
+    zero_val_data = np.array(np
+                        .zeros(wm_total_entries)
+                        .reshape(X_arr_col, max_iterations))
+    zero_test_data = np.array(np
+                        .zeros(wm_total_entries)
+                        .reshape(X_arr_col, max_iterations))
+
+    one_train_data = np.array(np
+                        .zeros(wm_total_entries)
+                        .reshape(X_arr_col, max_iterations))
+    one_val_data = np.array(np
+                        .zeros(wm_total_entries)
+                        .reshape(X_arr_col, max_iterations))
+    one_test_data = np.array(np
+                        .zeros(wm_total_entries)
+                        .reshape(X_arr_col, max_iterations))
+
+
+    # np.random.shuffle(data_matrix_test)
+
+    # apply gradient descent to matrix
+    learned_weight_matrix = gradientDescent(data_matrix_test, binary_vector, .05, 100)
+    
 
 
 
-print(gradientDescent(data_matrix_test[:500 , :],binary_vector,.05,100))
-
+# call our main
+main()
