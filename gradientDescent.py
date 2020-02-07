b47856508085080f1cc415f1fcd7850c1c2be11b
Jamp-H <jmh824@nau.edu>
2020-02-06 23:33:21 -0700

    Please figure out the 3 different sets after i randomize and scale the array and the graphing

--- gradientDescent.py
+++ gradientDescent.py
@@ -30,36 +30,38 @@ def gradientDescent(X, y, step_size, max_iterations):
                         .zeros(wm_total_entries)
                         .reshape(X_arr_col, max_iterations))
 
-
     # ALGORITHM
-    for index in range(0, max_iterations):
+    weight_vector_transpose = np.transpose(weight_vector)
+
+    for iteration in range(0, max_iterations):
         #calculate y_tid
-        y_tild = -1
+        for index in range(0, X.shape[1]):
 
-        if(y[index] == 1):
-            y_tild = 1
+            grad_log_losss = 0
+            verctor_mult = 0
+            inner_exp = 0
 
-        # variables for simplification
-        weight_vector_transpose = np.transpose(weight_vector)
-        verctor_mult = np.multiply(weight_vector_transpose, X[index,:])
-        inner_exp = np.multiply(y_tild, verctor_mult)
-        # calculate gradient
+            y_tild = -1
 
-        gradient = (1/(1+np.exp(inner_exp))*
-                    (np.exp(inner_exp))*
-                    ((-y_tild)*X[index,:]))
+            if(y[index] == 1):
+                y_tild = 1
 
+            # variables for simplification
+            verctor_mult = np.multiply(weight_vector_transpose, X[index,:])
+            inner_exp = np.multiply(y_tild, verctor_mult)
+            # calculate gradient
+
+            gradient = (1/(1+np.exp(inner_exp))*
+                        (np.exp(inner_exp))*
+                        ((-y_tild)*X[index,:]))
+
+            grad_log_losss += gradient
+
+        mean_grad_log_loss = grad_log_losss/X.shape[1]
 
         # update weight_vector depending on positive or negative
-        # If negative, you add to the steps
-        if y_tild == -1:
-            weight_vector += gradient
+        weight_vector = weight_vector - np.multiply(step_size, mean_grad_log_loss)
 
-        # If positive, you subtract it
-        elif y_tild == 1:
-            weight_vector -= gradient
-
-        #print(weight_vector)
         # store the resulting weight_vector in the corresponding column weight_matrix
         weight_matrix[: ,index] = weight_vector
 
@@ -85,11 +87,8 @@ def scale(matrix):
 
         std = sqrt(col_sq_sum/col_size)
 
-        print(f"{counter} : mean {mean} std: {std}")
         column -= mean
-        #print(f"col - {column}\n")
         column /= std
-        #print(f"col / {column}")
 
 
 def convert_data_to_matrix(file_name):
@@ -99,17 +98,24 @@ def convert_data_to_matrix(file_name):
     data_matrix_full = np.array(spam_file[0:], dtype=np.float)
     return data_matrix_full
 
-data_matrix_full = convert_data_to_matrix("spam.data")
+def main():
+    data_matrix_full = convert_data_to_matrix("spam.data")
 
-col_length = data_matrix_full.shape[1]
+    np.random.shuffle(data_matrix_full)
 
-data_matrix_test = np.delete(data_matrix_full, col_length - 1, 1)
+    col_length = data_matrix_full.shape[1]
 
-binary_vector = data_matrix_full[:,57]
+    data_matrix_test = np.delete(data_matrix_full, col_length - 1, 1)
 
-scale(data_matrix_test)
+    binary_vector = data_matrix_full[:,57]
 
+    scale(data_matrix_test)
 
+    weight_matrix = gradientDescent(data_matrix_test,binary_vector,.05,500)
 
-gradientDescent(data_matrix_test,binary_vector,.05,100)
+    print(np.sum(weight_matrix))
+
+    np.savetxt("plzwork.csv", weight_matrix, delimiter = " ")
+
+main()
 
