d868f06ed669bb1f1545556eae6887a9292055ed
Jacob-D-Christiansen <Jacob.D.Christiansen@gmail.com>
2020-02-14 17:22:55 -0700

    added the pdf of the paper

---
+++ RocPot.R
@@ -0,0 +1,8 @@
+library(WeightedROC)
+
+csvFile <- read.csv(file="/home/jacob/Documents/Cs_499/GradientDescent/spamROC.csv", header = TRUE)
+baseline=seq(from=0,to=1)
+tittle <- "ROC curve of linear regression Vs Baseline"
+plot(x=csvFile$TPR,y=csvFile$FPR, type="l",col="red", main=tittle,xlab="FPR",ylab="TPR")
+lines(x=baseline,y=baseline, col="blue")
+legend(x=0,y=1,legend=c("Regression", "BaseLine"), col=c("red","blue"), lty=1)
